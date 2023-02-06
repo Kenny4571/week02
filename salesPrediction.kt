d2f17ae936f5895ae5f21f0d646e3c41dde038e5
Kenny4571 <119124617+Kenny4571@users.noreply.github.com>
2023-02-06 14:57:17 -0600

    salesPrediction.kt

---
+++ salesPrediction.kt
@@ -0,0 +1,5 @@
+val companySales = 4.6
+val eastCoastDivisionPercentage = 0.62
+val eastCoastDivisionSales = companySales * eastCoastDivisionPercentage
+
+println("The East Coast division will generate \$$eastCoastDivisionSales this year.")
